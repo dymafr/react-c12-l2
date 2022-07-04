675b8e784f192188f2e7fa6c370785756d7cac7a
dymafr <erwan1309@hotmail.com>
2022-07-04 14:51:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-swptq7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-swptq7)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import HomePage from './pages/HomePage/HomePage';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
